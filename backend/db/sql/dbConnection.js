1e8881e018dfea85e0aebc1725a026d29b101e03
Rab0129 <workingrikuagogo@gmail.com>
2025-05-20 22:52:26 +0900

    DBの接続先を環境変数に変更

--- backend/db/sql/dbConnection.js
+++ backend/db/sql/dbConnection.js
@@ -1,16 +1,10 @@
 const Pool = require('pg-pool');
+//dotenvを追加して読み込み
+require('dotenv').config();
 
-// 環境変数を使用してセキュアに設定
-const dbConfig = {
-    database: process.env.DB_NAME || 'test_yasai',
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5432,
-};
+// 環境変数(.envから)を使用してセキュアに設定
+const connectionString = process.env.POSTGRES_URL;
 
-//直接もろもろ指定したらしい
-const connectionString = "postgres://postgres:postgres@postgres:5432/test_yasai"
 // コネクションプールの作成
 const pool = new Pool({ connectionString });
 
