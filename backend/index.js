41cf2aa9b0b49ddbed98dfe32419b2b014843322
Rab0129 <workingrikuagogo@gmail.com>
2025-05-22 23:14:52 +0900

    受け取った値をSQLで検索できる処理を追加しました。

--- backend/index.js
+++ backend/index.js
@@ -3,26 +3,46 @@ const express = require('express');
 const app = express();
 const pool = require('./db/sql/dbConnection');
 
-/* GET home page. */
-app.get('/', (req, res) => {            //topだよ
-    res.send('Hello World!');
-});
-
 // 非同期処理を扱うためにasyncを追加
-app.post('/', async (req, res, next) => {
+app.get('/', async (req, res, next) => {
     try {
         // データベースを参照するためのクエリ（処理を書けばなんでも行ける）
         const selectTable = 'SELECT id,title,memo FROM leaf_yasai';
         // データベースへの挿入を非同期に実行　（格納）
-        const s = await pool.query(selectTable, []);
+        const selectAll = await pool.query(selectTable);
         // teble表示
-        res.send(s.rows);  //sの中のrowを提出
+        res.send(selectAll.rows);  //sの中のrowを提出
+        console.log(selectAll);
     } catch (error) {
         console.error(error);
         // エラーハンドリング: サーバーエラーが発生した場合は500をクライアントに返す
         res.status(500).send('サーバーエラー');
     }
 });
+
+app.get('/search', async (req, res, next) => {
+    try {
+        //クエリで受け取った値をSQLと絡めて変数に格納
+        const search = 'SELECT id,title,memo FROM leaf_yasai WHERE title=$1';
+
+        //受け取ったタイトルをvaluesに入れるよ--$1で認識 
+        //------ToDo(ifでnullの確認を行いたい)-----------
+        const title = req.query.title;
+        const values = [title];
+
+        //poolへSQL宣言したものをもってきてupに格納
+        const results = await pool.query(search, values);
+        //htmlにsendしているよね
+        res.send(results.rows);
+        //logに出力
+        console.log(results);
+    } catch (error) {
+        console.error(error);
+        // エラーハンドリング: サーバーエラーが発生した場合は500をクライアントに返す
+        res.status(500).send('サーバーエラー');
+    }
+});
+
 
 app.listen(3333, () => {                                //受付中 立ち上がった状態でリクエストをまつよ
     console.log('Server listening on port 3333');
